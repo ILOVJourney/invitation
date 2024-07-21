62781f48de451cc565dfe96cbd14dd271c661682
myeongju-kim <mjoo1106@naver.com>
2024-07-21 22:32:14 +0900

    Deploy

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  <router-link to="/"></router-link>
-  <router-link to="/home"></router-link>
-  <router-view></router-view>
+  <img src="../public/intro.png" v-if = "isIntroView" @click="clickHandler">
+  <HomePage v-if="!isIntroView"/>
 </template>
 
 <script>
+import HomePage from './pages/home-page.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isIntroView: true,
+    };
+  },
+  methods:{
+    clickHandler(){
+      console.log("TEST")
+      this.isIntroView = false
+    },
+  },
+  components:{
+    HomePage,
+  }
 }
 </script>
+<style scoped>
+  img{
+    position:absolute; 
+    width: 100%;
+    left:50%;
+    top:50%; 
+    transform:translateY(-50%) translateX(-50%);
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './index.js';
 
 const app = createApp(App);
-app.use(router);
 app.mount('#app')
